8464581caed3d9bf8fdaa6d504c2626db7dce702
GenTimms <gen@gentimms.com>
2018-06-22 21:30:45 +0100

    Created run model and main table view

---
+++ StravaBests/Run.swift
@@ -0,0 +1,19 @@
+//
+//  Run.swift
+//  StravaBests
+//
+//  Created by Genevieve Timms on 22/06/2018.
+//  Copyright © 2018 GMJT. All rights reserved.
+//
+
+import Foundation
+
+struct Run {
+    let name: String
+    let date: Date
+    
+    let bests = [Distance: TimeInterval]()
+}
+
+
+
